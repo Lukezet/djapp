7d64527315fddd7dbba351b5f1c0a0c02908622e
Lukezet <lucasyanezbogni@hotmail.com>
2023-12-14 19:51:30 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+
+
+<template>
+  <button class="bg-red-500 font-semibold shadow-xl rounded-3xl text-3xl p-8">hello Wolfs!</button>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+/* .logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 3em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+} */
+</style>
